f6565bae0891948e225452b83560f75b6cc560f5
amine <108814041+amxnese@users.noreply.github.com>
2024-03-22 18:47:21 +0100

    modified 'view.py' and 'templates/blog/home.html' Files

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from datetime import datetime
+posts = [
+  {'author': 'amine',
+  'title': "first post's title",
+  'content': "first post's content",
+  'date': datetime.now()},
+  {'author': 'james',
+  'title': "second post's title",
+  'content': "second post's content",
+  'date': datetime.now()}
+]
 
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
